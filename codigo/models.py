b443f0e09d507088b0fbfb7008969cce9f376e8b
Henrique Oliveira <hog12247@gmail.com>
2026-08-24 18:49:32 -0300

    I finished the "cadastro" part.
    I finished the "cadastro" part.

--- codigo/models.py
+++ codigo/models.py
@@ -3,5 +3,6 @@ from extentions import db
 class Usuarios(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome_usuario = db.Column(db.String(100), nullable=False, unique=True)
+    email_usuario = db.Column(db.String(100), nullable=False, unique=True)
     senha_usuario = db.Column(db.String(100), nullable=False)
     
--- codigo/routes/usuarios.py
+++ codigo/routes/usuarios.py
@@ -1,4 +1,32 @@
 from flask import render_template, request, redirect, url_for, Blueprint
 from models import Usuarios
+from extentions import db
 
 usuarios_bp = Blueprint('usuarios', __name__)
+
+@usuarios_bp.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+
+
+        if Usuarios.query.filter((Usuarios.nome_usuario == nome) | (Usuarios.email_usuario == email)).first():
+            return render_template('cadastro.html', erro='Nome de usuário ou email já existe.')
+
+        novo_usuario = Usuarios(
+            nome_usuario=nome,
+            email_usuario=email,
+            senha_usuario=senha
+        )
+        
+        db.session.add(novo_usuario)
+        db.session.commit()
+
+    return render_template('cadastro.html')
+
+@usuarios_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
